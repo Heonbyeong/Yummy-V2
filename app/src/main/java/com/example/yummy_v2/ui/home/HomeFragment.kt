072ae0b510f1f6bd329f4a0094303af9dcce16b9
Anbyeonghun <abh0920one@gmail.com>
2022-03-28 23:22:30 +0900

    [Modify] Google maps api key

--- app/src/main/java/com/example/yummy_v2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/yummy_v2/ui/home/HomeFragment.kt
@@ -10,17 +10,24 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.yummy_v2.R
 import com.example.yummy_v2.base.BaseFragment
 import com.example.yummy_v2.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), OnMapReadyCallback {
 
+    private lateinit var mMap: GoogleMap
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val mapFragment = activity?.supportFragmentManager
+            ?.findFragmentById(R.id.google_map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 }
