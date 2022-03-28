9641e3d13af1cc9c9cb4296f3885c94fa0431ffc
Anbyeonghun <abh0920one@gmail.com>
2022-03-28 10:48:01 +0900

    [Style] 추천 bottom navigation icon 수정

--- app/src/main/java/com/example/yummy_v2/MainActivity.kt
+++ app/src/main/java/com/example/yummy_v2/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.yummy_v2
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.yummy_v2.base.BaseActivity
 import com.example.yummy_v2.databinding.ActivityMainBinding
@@ -25,7 +23,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard
+                R.id.navigation_home, R.id.navigation_recommend
             )
         )
         navView.setupWithNavController(navController)
