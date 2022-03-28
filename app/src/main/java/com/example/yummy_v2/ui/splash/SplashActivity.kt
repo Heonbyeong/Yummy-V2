58a121906464cd822cd8f76b13517cd62a851259
Anbyeonghun <abh0920one@gmail.com>
2022-03-28 22:03:43 +0900

    [Modify] Handler 추가

--- app/src/main/java/com/example/yummy_v2/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/yummy_v2/ui/splash/SplashActivity.kt
@@ -1,13 +1,29 @@
 package com.example.yummy_v2.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.yummy_v2.MainActivity
+import com.example.yummy_v2.R
 
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private val DURATION = 2000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            intent.apply {
+                Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            startActivity(intent)
+        }, DURATION)
     }
 }
