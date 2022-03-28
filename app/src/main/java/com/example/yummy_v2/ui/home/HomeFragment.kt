7a4c18d4f77c70e83720678d98f42bee8a445ad0
Anbyeonghun <abh0920one@gmail.com>
2022-03-28 23:56:10 +0900

    [Modify] Google map View

--- app/src/main/java/com/example/yummy_v2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/yummy_v2/ui/home/HomeFragment.kt
@@ -1,30 +1,51 @@
 package com.example.yummy_v2.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.yummy_v2.R
 import com.example.yummy_v2.base.BaseFragment
 import com.example.yummy_v2.databinding.FragmentHomeBinding
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mapView: MapView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        val mapFragment = activity?.supportFragmentManager
-            ?.findFragmentById(R.id.google_map) as SupportMapFragment
-            mapFragment.getMapAsync(this)
+        mapView = binding.googleMap
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
