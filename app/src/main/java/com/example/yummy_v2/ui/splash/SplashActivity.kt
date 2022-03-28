11819e3b9f4e2a7e8033ca72064febd5a827487e
Anbyeonghun <abh0920one@gmail.com>
2022-03-28 11:27:30 +0900

    [Create] Splash activity 추가

---
+++ app/src/main/java/com/example/yummy_v2/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.yummy_v2.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
