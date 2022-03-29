686853116e3e6ba5836c588eb9351fb9ef20fa99
Anbyeonghun <abh0920one@gmail.com>
2022-03-29 09:04:10 +0900

    [Modify] Permission 요청 메소드 추가

--- app/src/main/java/com/example/yummy_v2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/yummy_v2/ui/home/HomeFragment.kt
@@ -1,17 +1,25 @@
 package com.example.yummy_v2.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.yummy_v2.R
 import com.example.yummy_v2.base.BaseFragment
 import com.example.yummy_v2.databinding.FragmentHomeBinding
 import com.google.android.gms.maps.*
+import com.google.android.material.snackbar.Snackbar
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mapView: MapView
+    private val PERMISSIONS_REQUEST_CODE = 100
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -50,5 +58,34 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+
+    private fun checkPermission() {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if(hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED){
+            startLocationUpdates()
+        } else {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), REQUIRED_PERMISSIONS[0])){
+                Snackbar.make(binding.layoutHome, R.string.snack_bar_message,
+                    Snackbar.LENGTH_INDEFINITE).setAction("확인") {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(), REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE
+                    )
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE)
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+
     }
 }
