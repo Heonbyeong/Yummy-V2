eb8516fb4a2f11028ef886f393d1a4029d95b576
Anbyeonghun <abh0920one@gmail.com>
2022-03-31 22:29:01 +0900

    [Modify] Place marker icon custom

--- app/src/main/java/com/example/yummy_v2/network/PlacesAPI.kt
+++ app/src/main/java/com/example/yummy_v2/network/PlacesAPI.kt
@@ -1,13 +1,18 @@
 package com.example.yummy_v2.network
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.yummy_v2.BuildConfig
+import com.example.yummy_v2.R
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -83,6 +88,7 @@ class PlacesAPI(
         }
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun placeMarker() {
         for(marker: Marker in marker_list){
             marker.remove()
@@ -97,10 +103,20 @@ class PlacesAPI(
                     position(pos)
                     title(name_list[i])
                     snippet(vicinity_list[i])
+                    icon(bitmapDescriptorFromVector(mContext, R.drawable.ic_marker))
                 }
                 val marker = _map.addMarker(options)
                 marker_list.add(marker!!)
             }
         }
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int) : BitmapDescriptor{
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+        vectorDrawable!!.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
