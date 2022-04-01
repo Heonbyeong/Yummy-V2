c646c33e3d9d403bca0bcce287f822350af6b224
Anbyeonghun <abh0920one@gmail.com>
2022-04-01 21:43:21 +0900

    [Add] AddressViewModel

---
+++ app/src/main/java/com/example/yummy_v2/ui/home/AddressViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.yummy_v2.ui.home
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.yummy_v2.model.remote.AddressRepositoryImpl
+import com.example.yummy_v2.model.remote.AddressResponse
+import kotlinx.coroutines.launch
+
+class AddressViewModel : ViewModel() {
+    private val repository = AddressRepositoryImpl()
+    private val liveData: MutableLiveData<AddressResponse> = MutableLiveData()
+
+    fun searchAddress(
+        confmKey: String,
+        resultType: String,
+        currentPage: String,
+        countPerPage: String,
+        keyword: String
+    ) {
+        viewModelScope.launch {
+            val response = repository.searchAddress(confmKey, resultType, currentPage, countPerPage, keyword)
+            if(response.isSuccessful){
+                liveData.postValue(response.body())
+            }
+        }
+    }
+}
