57ca82276a73a7d279d0cbac4777f271b1446742
Anbyeonghun <abh0920one@gmail.com>
2022-04-01 19:58:02 +0900

    [Add] Repository 추가

--- app/src/main/java/com/example/yummy_v2/network/AddressAPI.kt
+++ app/src/main/java/com/example/yummy_v2/network/AddressAPI.kt
@@ -1,16 +1,17 @@
 package com.example.yummy_v2.network
 
+import com.example.yummy_v2.model.remote.AddressResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface AddressAPI {
-    @GET("addrlink/addrLinkApiJsonp.do")
+    @GET("addrlink/addrLinkApi.do")
     suspend fun searchAddress(
         @Query("confmKey") confmKey: String,
         @Query("resultType") resultType: String,
         @Query("currentPage") currentPage: String,
         @Query("countPerPage") countPerPage: String,
         @Query("keyword") keyword: String
-    ) : Response<String>
+    ) : Response<AddressResponse>
 }
