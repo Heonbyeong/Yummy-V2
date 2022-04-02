f1823c32272fc6d6239c8e6189af3a4cb8ed661c
Anbyeonghun <abh0920one@gmail.com>
2022-04-02 15:23:28 +0900

    [Modify] recyclerview

--- app/src/main/java/com/example/yummy_v2/ui/home/AddressActivity.kt
+++ app/src/main/java/com/example/yummy_v2/ui/home/AddressActivity.kt
@@ -2,28 +2,36 @@ package com.example.yummy_v2.ui.home
 
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.yummy_v2.BuildConfig
 import com.example.yummy_v2.R
 import com.example.yummy_v2.base.BaseActivity
 import com.example.yummy_v2.databinding.ActivityAddressBinding
 
 class AddressActivity : BaseActivity<ActivityAddressBinding>(R.layout.activity_address) {
-    private val viewModel = AddressViewModel()
+    private lateinit var addressViewModel : AddressViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.activity = this
 
-        val addressViewModel = ViewModelProvider(this).get(AddressViewModel::class.java)
+        addressViewModel = ViewModelProvider(this).get(AddressViewModel::class.java)
+        binding.addressRv.layoutManager = LinearLayoutManager(this)
 
         binding.addressEt.setOnEditorActionListener{v, actionId, event ->
             var handled = false
             if(actionId == EditorInfo.IME_ACTION_SEARCH){
-                viewModel.searchAddress(BuildConfig.ADDR_API_KEY,
-                    "json", 1, 30, binding.addressEt.getText().toString())
+                addressViewModel.searchAddress(BuildConfig.ADDR_API_KEY,
+                    "json", 1, 10, binding.addressEt.getText().toString())
                 handled = true
             }
             return@setOnEditorActionListener handled
         }
+
+        addressViewModel.liveData.observe(this, Observer {
+            val addressAdapter = AddressRVAdapter(addressViewModel.liveData)
+            binding.addressRv.adapter = addressAdapter
+        })
     }
 }
--- app/src/main/java/com/example/yummy_v2/ui/home/AddressViewModel.kt
+++ app/src/main/java/com/example/yummy_v2/ui/home/AddressViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.yummy_v2.ui.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,7 +10,8 @@ import kotlinx.coroutines.launch
 
 class AddressViewModel : ViewModel() {
     private val repository = AddressRepositoryImpl()
-    private val liveData: MutableLiveData<AddressResponse> = MutableLiveData()
+    private val _liveData: MutableLiveData<AddressResponse> = MutableLiveData()
+    val liveData : LiveData<AddressResponse> get() = _liveData
 
     fun searchAddress(
         confmKey: String,
@@ -21,7 +23,8 @@ class AddressViewModel : ViewModel() {
         viewModelScope.launch {
             val response = repository.searchAddress(confmKey, resultType, currentPage, countPerPage, keyword)
             if(response.isSuccessful){
-                liveData.postValue(response.body())
+                if(response.code() == 200)
+                    _liveData.postValue(response.body())
             }
         }
     }
