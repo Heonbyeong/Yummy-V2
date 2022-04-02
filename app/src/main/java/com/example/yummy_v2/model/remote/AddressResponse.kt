85054f80cf862eb5d163f256afac2486aefd7c31
Anbyeonghun <abh0920one@gmail.com>
2022-04-02 15:23:09 +0900

    [FIX] Json 수정

--- app/src/main/java/com/example/yummy_v2/model/remote/AddressResponse.kt
+++ app/src/main/java/com/example/yummy_v2/model/remote/AddressResponse.kt
@@ -1,27 +1,32 @@
 package com.example.yummy_v2.model.remote
 
 data class AddressResponse(
-    val results : CommonResponse,
-    val juso : ArrayList<JusoResponse>
+    val results : ResultResponse,
 ) {
-    data class CommonResponse(
-        val totalCount: String,
-        val currentPage: String,
-        val countPerPage: String,
-        val errorCode: String,
-        val errorMessage: String,
-    )
-    data class JusoResponse(
-        val roadAddr: String,
-        val roadAddrPart1: String,
-        val roadAddrPart2: String,
-        val jibunAddr: String,
-        val engAddr: String,
-        val zipNo: String,
-        val admCd: String,
-        val rnMgtSn: String,
-        val bdMgtSn: String,
-        val detBdNmList: String,
-        val bdNm: String
-    )
+    data class ResultResponse(
+        val common : CommonResponse,
+        val juso : ArrayList<JusoResponse>
+    ) {
+        data class CommonResponse(
+            val totalCount: String,
+            val currentPage: String,
+            val countPerPage: String,
+            val errorCode: String,
+            val errorMessage: String,
+        )
+
+        data class JusoResponse(
+            val roadAddr: String,
+            val roadAddrPart1: String,
+            val roadAddrPart2: String,
+            val jibunAddr: String,
+            val engAddr: String,
+            val zipNo: String,
+            val admCd: String,
+            val rnMgtSn: String,
+            val bdMgtSn: String,
+            val detBdNmList: String,
+            val bdNm: String
+        )
+    }
 }
