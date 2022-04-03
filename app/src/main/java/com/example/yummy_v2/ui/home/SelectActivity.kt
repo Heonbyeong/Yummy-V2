465bc39ae63a38189f1887e7db1b33a3dbd956f2
Anbyeonghun <abh0920one@gmail.com>
2022-04-03 14:32:50 +0900

    [Remove] place picker 삭제

--- app/src/main/java/com/example/yummy_v2/ui/home/SelectActivity.kt
+++ app/src/main/java/com/example/yummy_v2/ui/home/SelectActivity.kt
@@ -4,11 +4,51 @@ import android.os.Bundle
 import com.example.yummy_v2.R
 import com.example.yummy_v2.base.BaseActivity
 import com.example.yummy_v2.databinding.ActivitySelectBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class SelectActivity : BaseActivity<ActivitySelectBinding>(R.layout.activity_select) {
+class SelectActivity : BaseActivity<ActivitySelectBinding>(R.layout.activity_select), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var mapView: MapView
+    private lateinit var mFusedLocationClient : FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
         binding.activity = this
+
+        mapView = binding.googleMap
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this@SelectActivity)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
     }
 }
