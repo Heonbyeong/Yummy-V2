1d71dcfdf2e475d637968555b7bd2ccd4923b0b2
Anbyeonghun <abh0920one@gmail.com>
2022-04-07 19:11:07 +0900

    [Modify] AddressRepository interface 수정

--- app/src/main/java/com/example/yummy_v2/data/remote/AddressRepository.kt
+++ app/src/main/java/com/example/yummy_v2/data/remote/AddressRepository.kt
@@ -1,13 +1,24 @@
 package com.example.yummy_v2.data.remote
 
+import com.example.yummy_v2.network.AddressAPI
+import com.example.yummy_v2.network.RetrofitBuilder
+import com.example.yummy_v2.network.SafeApiCall
 import retrofit2.Response
+import javax.inject.Inject
 
-interface AddressRepository {
+class AddressRepository @Inject constructor(
+    private val retrofit : AddressAPI
+) :SafeApiCall() {
     suspend fun searchAddress(
         confmKey: String,
         resultType: String,
         currentPage: Int,
         countPerPage: Int,
         keyword: String
-    ) : Response<AddressResponse>
+    ): Response<AddressResponse> {
+        return safeApiCall {
+            retrofit.searchAddress(confmKey, resultType, currentPage, countPerPage, keyword)
+        }
+    }
+
 }
