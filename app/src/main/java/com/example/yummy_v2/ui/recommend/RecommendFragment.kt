ce9136557a8679b5ae2a4f208e05b0148a2e618f
Anbyeonghun <abh0920one@gmail.com>
2022-04-09 23:26:45 +0900

    [Modify] Recommend algorithem

--- app/src/main/java/com/example/yummy_v2/ui/recommend/RecommendFragment.kt
+++ app/src/main/java/com/example/yummy_v2/ui/recommend/RecommendFragment.kt
@@ -1,22 +1,52 @@
 package com.example.yummy_v2.ui.recommend
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.example.yummy_v2.R
 import com.example.yummy_v2.base.BaseFragment
+import com.example.yummy_v2.data.local.Place
 import com.example.yummy_v2.databinding.FragmentRecommendBinding
+import com.example.yummy_v2.util.BottomSheetDialogUtil
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
 
+    private lateinit var bottomSheet : BottomSheetDialogUtil
     private val recommendViewModel : RecommendViewModel by viewModels()
+    private var restaurantsList : List<Place>? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        val a = recommendViewModel.getAll()
+        binding.fragment = this
+        bottomSheet = BottomSheetDialogUtil(requireActivity())
+        recommendViewModel.getAll()
+
+        recommendViewModel.restaurants.observe(viewLifecycleOwner) {
+            restaurantsList = it
+        }
+    }
+
+    fun recommend() {
+        if(restaurantsList != null){
+            binding.blur.visibility = View.VISIBLE
+            binding.lottieView.visibility = View.VISIBLE
+            val range = (0 until restaurantsList!!.size).random()
+            val restaurants = restaurantsList!![range]
+
+            requireActivity().runOnUiThread{
+                Handler(Looper.myLooper()!!).postDelayed({
+                    binding.blur.visibility = View.INVISIBLE
+                    binding.lottieView.visibility = View.INVISIBLE
+                    val dialog = bottomSheet.setUpDialog(R.layout.restaurants_bottom)
+                    bottomSheet.binding2.nameTv.text = restaurants.name
+                    bottomSheet.binding2.bottomAddrTv.text = restaurants.address
+                    dialog.show()
+                }, 2000L)
+            }
+        }
     }
 }
