f6ee17c3b57cb66306ad5a439b7076f7232042b2
Anbyeonghun <abh0920one@gmail.com>
2022-04-09 15:03:35 +0900

    [Modify] room db 수정

--- app/src/main/java/com/example/yummy_v2/data/local/PlaceDatabase.kt
+++ app/src/main/java/com/example/yummy_v2/data/local/PlaceDatabase.kt
@@ -1,30 +1,9 @@
 package com.example.yummy_v2.data.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [Place::class], version = 1)
 abstract class PlaceDatabase : RoomDatabase() {
     abstract fun placeDao() : PlaceDao
-
-    companion object {
-        private var instance: PlaceDatabase? = null
-
-        @OptIn(InternalCoroutinesApi::class)
-        @Synchronized
-        fun getInstance(context: Context): PlaceDatabase? {
-            synchronized(PlaceDatabase::class) {
-                instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    PlaceDatabase::class.java,
-                    "place-database"
-                ).build()
-            }
-            return instance
-        }
-    }
 }
