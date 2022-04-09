d3c9269fcef22125ffffccb00e3bbfae23613106
Anbyeonghun <abh0920one@gmail.com>
2022-04-09 23:25:48 +0900

    [Modify] BottomSheetDialogUtil setup method add return

--- app/src/main/java/com/example/yummy_v2/util/BottomSheetDialogUtil.kt
+++ app/src/main/java/com/example/yummy_v2/util/BottomSheetDialogUtil.kt
@@ -5,17 +5,28 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.yummy_v2.R
 import com.example.yummy_v2.databinding.AddressBottomBinding
+import com.example.yummy_v2.databinding.RestaurantsBottomBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class BottomSheetDialogUtil(private val activity: Activity) {
 
     lateinit var binding : AddressBottomBinding
+    lateinit var binding2 : RestaurantsBottomBinding
 
     fun setUpDialog(resId: Int) : BottomSheetDialog {
-        val view = activity.layoutInflater.inflate(resId, null)
-        binding = AddressBottomBinding.inflate(activity.layoutInflater, view as ViewGroup, false)
         val dialog = BottomSheetDialog(activity)
-        dialog.setContentView(binding.root)
+        when(resId) {
+            R.layout.address_bottom -> {
+                val view = activity.layoutInflater.inflate(resId, null)
+                binding = AddressBottomBinding.inflate(activity.layoutInflater, view as ViewGroup, false)
+                dialog.setContentView(binding.root)
+            }
+            R.layout.restaurants_bottom -> {
+                val view = activity.layoutInflater.inflate(resId, null)
+                binding2 = RestaurantsBottomBinding.inflate(activity.layoutInflater, view as ViewGroup, false)
+                dialog.setContentView(binding2.root)
+            }
+        }
         return dialog
     }
 }
