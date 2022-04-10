909ead8753021c75a364b77afb8bb34a6215544c
Anbyeonghun <abh0920one@gmail.com>
2022-04-10 22:29:12 +0900

    [Add] Google map 길찾기 intent open

--- app/src/main/java/com/example/yummy_v2/ui/recommend/RecommendFragment.kt
+++ app/src/main/java/com/example/yummy_v2/ui/recommend/RecommendFragment.kt
@@ -1,8 +1,11 @@
 package com.example.yummy_v2.ui.recommend
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.yummy_v2.R
@@ -10,7 +13,9 @@ import com.example.yummy_v2.base.BaseFragment
 import com.example.yummy_v2.data.local.Place
 import com.example.yummy_v2.databinding.FragmentRecommendBinding
 import com.example.yummy_v2.util.BottomSheetDialogUtil
+import com.example.yummy_v2.util.PrefsHelper
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
@@ -19,9 +24,13 @@ class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragme
     private val recommendViewModel : RecommendViewModel by viewModels()
     private var restaurantsList : List<Place>? = null
 
+    @Inject
+    lateinit var prefs : PrefsHelper
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fragment = this
+        prefs
         bottomSheet = BottomSheetDialogUtil(requireActivity())
         recommendViewModel.getAll()
 
@@ -44,9 +53,28 @@ class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragme
                     val dialog = bottomSheet.setUpDialog(R.layout.restaurants_bottom)
                     bottomSheet.binding2.nameTv.text = restaurants.name
                     bottomSheet.binding2.bottomAddrTv.text = restaurants.address
+                    bottomSheet.binding2.confirmBtn.setOnClickListener{
+                        googleMapIntent(
+                            prefs.curLat!!.toDouble(),
+                            prefs.curLng!!.toDouble(),
+                            restaurants.lat,
+                            restaurants.lng)
+                    }
                     dialog.show()
                 }, 2000L)
             }
         }
+    }
+
+    private fun googleMapIntent(
+        startLat: Double,
+        startLng: Double,
+        endLat: Double,
+        endLng: Double) {
+        val uri = "http://maps.google.com/maps?saddr=$startLat,$startLng&daddr=$endLat,$endLng"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri)).apply {
+            `package` = "com.google.android.apps.maps"
+        }
+        startActivity(intent)
     }
 }
